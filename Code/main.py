30a67e8fa7cb67d8e56d0e8963c202c057559fad
Oixel Romero <oycell@aol.com>
2026-03-03 10:46:06 -0500

    add test script for thermal printer

---
+++ Code/main.py
@@ -0,0 +1,12 @@
+from escpos.printer import Serial
+
+if __name__ == "__main__":
+    p = Serial(devfile='/dev/serial0,
+           baudrate=9600,
+           bytesize=8,
+           parity='N',
+           stopbits=1,
+           timeout=1.00,
+           dsrdtr=True)
+    
+    p.text("Hello World\n")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # photobooth
 An open source photobooth made with a Raspberry Pi Zero.
+
+## Packages Used:
+- [python-escpos](https://python-escpos.readthedocs.io/en/latest/) (Thermal Printer Control)
