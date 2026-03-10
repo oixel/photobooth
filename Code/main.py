a8907b54161f60e673cb0ff7b628bd4c43b6cc4c
ArtsyAnt <antonio.mariomd@gmail.com>
2026-03-10 13:25:26 -0400

    updated camera function and rearranged some files.

--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,20 @@
 from escpos.printer import Serial
-
+# temp to test
+from escpos.printer import Usb
 if __name__ == "__main__":
-    p = Serial(devfile='/dev/serial0',
-           baudrate=9600,
-           bytesize=8,
+    # p = Serial(devfile='/dev/serial0',
+    #        baudrate=9600,
+    #        bytesize=8,
+    #        parity='N',
+    #        stopbitss=1,
+    #        timeout=1.00,
+    #        dsrdtr=True)
+
+    p = Serial(devfile='COM3', baudrate=9600, 
            parity='N',
-           stopbits=1,
+           stopbitss=1,
            timeout=1.00,
            dsrdtr=True)
     
-    p.image("test_image.jpg")
+    output_image = "C:/Users/mario/Coding/photobooth/Code/images/output.jpg"
+    p.image(output_image)
